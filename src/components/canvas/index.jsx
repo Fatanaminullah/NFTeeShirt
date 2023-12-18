820a60383fbaabccddb97eadc4615f9313a1bc5e
Fatan Aminullah <fatan.aminullah.j@gmail.com>
2023-12-18 14:54:19 +0700

    feat: download and verification process

--- src/components/canvas/index.jsx
+++ src/components/canvas/index.jsx
@@ -5,13 +5,19 @@ import Shirt from "./Shirt";
 import Backdrop from "./Backdrop";
 import CameraRig from "./CameraRig";
 
-function CanvasModel() {
+function CanvasModel({ ref }) {
   return (
     <Canvas
+      ref={ref}
+      id="shirt-display"
       className="canvas-model-wrapper w-full max-w-full h-full transition-all ease-in"
       shadows
       camera={{ position: [0, 0, 0], fov: 30 }}
       gl={{ preserveDrawingBuffer: true }}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        return false;
+      }}
     >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
